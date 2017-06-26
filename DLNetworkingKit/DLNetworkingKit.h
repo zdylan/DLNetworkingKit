b59969ec9cc7d206c1439b8553828f0ce91ac691
dylan_zhang_ <dylan.zhang111@gmail.com>
2017-06-27 00:27:36 +0800

    Add Initial Version.

---
+++ DLNetworkingKit/DLNetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  DLNetworkingKit.h
+//  DLNetworkingKit
+//
+//  Created by dylan.zhang on 2017/6/27.
+//  Copyright © 2017年 dylan.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLNetworkingKit.
+FOUNDATION_EXPORT double DLNetworkingKitVersionNumber;
+
+//! Project version string for DLNetworkingKit.
+FOUNDATION_EXPORT const unsigned char DLNetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLNetworkingKit/PublicHeader.h>
+
+
